e59d3354dadfbfc9096829dc98e608755a7a2d2c
admin <37464913+shivam3898@users.noreply.github.com>
2019-10-25 12:24:04 +0530

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,27 @@
             interval: 3000
         }); 
         $('.carousel').carousel('cycle');
- }); 
+		
+
+		var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
+	$(window).scroll(function() {
+		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+			$(".counter").each(function() {
+				var element = $(this);
+				var endVal = parseInt(element.text());
+
+				element.countup(endVal);
+			})
+
+			countUpFinished = true;
+
+		}
+
+
+	});
+
+ });
 
 
 
@@ -75,4 +95,6 @@
 
         */
       
+	  
+	  
       });
